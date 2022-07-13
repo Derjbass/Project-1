2611f465d9b68fc4bcbc4380731de22fb4552e35
eden <85859364+eden-jpg@users.noreply.github.com>
2022-07-13 14:23:38 -0500

    merging

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,6 +100,24 @@ function displayData(filteredMovies, rating, id) {
     }
     
 }
+// var allScores = JSON.parse(localStorage.getItem("scores"));
+
+// console.log(allScores)
+
+// var ul = document.querySelector("#scores");
+
+// for (let i = 0; i < allScores.length; i++) {
+
+//     // create an li element
+//     var li = document.createElement("li");
+
+//     // change text of li
+//     li.textContent = allScores[i].initials + " - " + allScores[i].score
+
+//     // add this li inside of the ul
+//     ul.append(li)
+    
+// }
 
 
 
