dcbb0008d0db2910511890f75924bf0eeb6eb7a2
joostyn <justyn.helgeson@gmail.com>
2022-07-13 14:49:20 -0500

    merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,18 @@ var imdbRating = [];
 var searchHistory = [];
 var ytLink = 'https://www.youtube.com/watch?v=';
 const ytApiKey = 'AIzaSyBZ4n5tgZISeBzztKLEue0Ap8w8lAl5XE8';
-const ottApiKey = 'bcf969b618msh00f834b35745ea6p1b0e5djsnf105dedaa30c'
+const ottApiKey = '1ad9febe11msh92b2745fa5a97f6p1c51cbjsn0fbca5480e70'
 let filteredMovies;
 
 //search button listener
 $('#btn').on('click', function (event) {
     //stop page refresh when search button is clicked
     event.preventDefault();
-
-    //loadClient().then(fetchData(movieName));
+    console.log("click");
 
     //add input field value to variable for fetch later
     movieName = $('input').val();
+    console.log(movieName);
     fetchMovieData(movieName);
 
 })
@@ -33,25 +33,22 @@ async function fetchMovieData(movie) {
     trailerId = [];
 
     //filter out movies that don't match the first word or first and second word
-    filteredMovies = ottData.results.filter(function (movie){
-        // console.log(movieName.split(' ').length - 1);
-        //console.log(movieName.split(' ')[movie.title.split(' ').length - 1].toLowerCase());
-        if(movie.title.split(' ')[0].toLowerCase() === movieName.split(' ')[0].toLowerCase() && movie.title.split(' ')[movieName.split(' ').length - 1].toLowerCase() === movieName.split(' ')[movieName.split(' ').length - 1].toLowerCase()){
-            // console.log('movie.title.length', movie.title.split(' ').length - 1);
+    filteredMovies = ottData.results.filter(function (movie) {
+
+        if (movie.title.split(' ')[0].toLowerCase() === movieName.split(' ')[0].toLowerCase() && movie.title.split(' ')[movieName.split(' ').length - 1].toLowerCase() === movieName.split(' ')[movieName.split(' ').length - 1].toLowerCase()) {
             return movie;
-        }else if(movie.title.split(' ')[0].toLowerCase() === movieName.split(' ')[0].toLowerCase()){
+        } else if (movie.title.split(' ')[0].toLowerCase() === movieName.split(' ')[0].toLowerCase()) {
             return movie;
         }
-    }).slice(0,3);
+    }).slice(0, 3);
 
     //get IDs for full array
-    for (var i = 0; i < filteredMovies.length; i++){
+    for (var i = 0; i < filteredMovies.length; i++) {
         imdbID[i] = filteredMovies[i].imdbid;
-        // console.log(imdbID);
     }
-    
+
     //fetch youtube trailer video ID
-    for (var i = 0; i < filteredMovies.length; i++){
+    for (var i = 0; i < filteredMovies.length; i++) {
         const ytUrl = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&type=video&part=snippet&q=${filteredMovies[i].title} + ' trailer'}`;
         const ytResponse = await fetch(ytUrl);
         const ytData = await ytResponse.json();
@@ -59,47 +56,40 @@ async function fetchMovieData(movie) {
     }
 
     //fetch imdb rating data
-    async function fetchImdbRating(index)
-    {
+    async function fetchImdbRating(index) {
         var imbdIDUrl = `https://ott-details.p.rapidapi.com/gettitleDetails?rapidapi-key=${ottApiKey}&imdbid=${imdbID[index]}`;
         var imdbIDResponse = await fetch(imbdIDUrl);
         var imdbData = await imdbIDResponse.json();
         imdbRating.push(imdbData.imdbrating);
         console.log(imdbData.imdbrating);
 
-        if(imdbData.imdbrating){
+        if (imdbData.imdbrating) {
             displayData(index, imdbRating[index], trailerId[index]);
         }
 
 
-         
+
     }
 
-    const delay = (time) => new Promise(res => setTimeout(res, time)); 
+    const delay = (time) => new Promise(res => setTimeout(res, time));
     $("#display-results-here").empty();
     for (let i = 0; i < filteredMovies.length; i++) {
         await delay(1500);
         fetchImdbRating(i);
-    // console.log(filteredMovies);
     }
 }
 
 //function to display data
 function displayData(index, rating, id) {
-    console.log('index: ' + index, rating, id);
-
     var title = filteredMovies[index].title;
-    
 
-        // var poster = ottData.results[i].imageurl[0];
+    try {
+        var poster = filteredMovies[index].imageurl[0]
+    } catch (error) {
+        var poster = './assets/images/no-image-icon-23500.jpg';
+    }
 
-        try {
-            var poster = filteredMovies[index].imageurl[0]
-        } catch (error) {
-            var poster = './assets/images/no-image-icon-23500.jpg';
-        }
-
-        $("#display-results-here").append(
+    $("#display-results-here").append(
         `<div class="text-center movie-card">
             <h2>${title}</h2>
             <img src="${poster}" alt="${title}" width="250" height="300">
@@ -110,23 +100,23 @@ function displayData(index, rating, id) {
 
         </div>`)
 
-        var storageObj = {
-            title: title,
-            poster: poster,
-            rating: rating,
-            trailer: `https://www.youtube.com/embed/${id}`
-        }
-        window.localStorage.clear();
-        searchHistory.push(storageObj);
-        localStorage.setItem("Search History", JSON.stringify(searchHistory));
+    var storageObj = {
+        title: title,
+        poster: poster,
+        rating: rating,
+        trailer: `https://www.youtube.com/embed/${id}`
+    }
+    window.localStorage.clear();
+    searchHistory.push(storageObj);
+    localStorage.setItem("Search History", JSON.stringify(searchHistory));
 }
 
 function showLastSearch() {
     var lastSearch = JSON.parse(localStorage.getItem("Search History"));
-    console.log(lastSearch);
-    for (var i = 0; i < lastSearch.length; i++){
+    
+    for (var i = 0; i < lastSearch.length; i++) {
         $("#display-results-here").append(
-        `<div class="text-center movie-card">
+            `<div class="text-center movie-card">
             <h2>${lastSearch[i].title}</h2>
             <img src="${lastSearch[i].poster}" alt="${lastSearch[i].title}" width="250" height="300">
             <h3>IMDB Rating: ${lastSearch[i].rating}</h3>
@@ -134,9 +124,8 @@ function showLastSearch() {
             src="${lastSearch[i].trailer}">
             </iframe> 
 
-        </div>`)}
+        </div>`)
+    }
 }
 
-
 showLastSearch();
-
